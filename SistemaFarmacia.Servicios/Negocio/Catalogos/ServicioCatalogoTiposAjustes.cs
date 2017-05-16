6d1eb17d3c46492c40e78e68c1c3fc9559fb6ff6
Javier Alvarez <proinnotec@gmail.com>
2017-05-15 23:16:25 -0500

    ServicioCatalogoTipoAjustes
    Se agrega la clase y la estructura inicial de la carpeta de negocio en
    servicios

---
+++ SistemaFarmacia.Servicios/Negocio/Catalogos/ServicioCatalogoTiposAjustes.cs
@@ -0,0 +1,21 @@
+﻿using ProInnotec.Core.Entidades.Datos;
+using ProInnotec.Core.Entidades.ManejoExcepciones;
+using SistemaFarmacia.Entidades.Negocio;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using SistemaFarmacia.Entidades.Negocio.Catalogos;
+
+namespace SistemaFarmacia.Servicios.Negocio.Catalogos
+{
+    public class ServicioCatalogoTiposAjustes
+    {
+        private IBaseDeDatos _baseDatos;
+
+
+        public ServicioCatalogoTiposAjustes(IBaseDeDatos baseDatos)
+        {
+            _baseDatos = baseDatos;
+        }
+    }
+}
