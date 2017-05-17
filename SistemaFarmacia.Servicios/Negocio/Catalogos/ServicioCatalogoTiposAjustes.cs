1b09237a9e76f48a5141e467ab3ffd232b2c9c38
Javier Alvarez <proinnotec@gmail.com>
2017-05-16 21:50:34 -0500

    Se agrega Servicio de Tipos de Ajustes Catálogo

--- SistemaFarmacia.Servicios/Negocio/Catalogos/ServicioCatalogoTiposAjustes.cs
+++ SistemaFarmacia.Servicios/Negocio/Catalogos/ServicioCatalogoTiposAjustes.cs
@@ -12,10 +12,73 @@ namespace SistemaFarmacia.Servicios.Negocio.Catalogos
     {
         private IBaseDeDatos _baseDatos;
 
+        public List<CatTipoAjustes> ListaTipoAjustes { get; private set; }
 
         public ServicioCatalogoTiposAjustes(IBaseDeDatos baseDatos)
         {
             _baseDatos = baseDatos;
+
+        }
+
+        public ExcepcionPersonalizada ConsultarTiposAjustes(CatTipoAjustes catalogoAjustes)
+        {
+            IDbConnection conexion = null;
+
+            ListaTipoAjustes = new List<CatTipoAjustes>();
+
+            try
+            {
+                conexion = _baseDatos.CrearConexionAbierta();
+                IDbCommand comando = _baseDatos.CrearComandoStoredProcedure("spS_CatTipoAjustes", conexion);
+
+                if ( catalogoAjustes.EsActivo )
+                {
+                    IDataParameter parametroEsActivo = _baseDatos.CrearParametro("@EsActivo", catalogoAjustes.EsActivo, ParameterDirection.Input);
+                }
+
+                if ( catalogoAjustes.IdTipoAjuste > 0)
+                {
+                    IDataParameter parametroEsActivo = _baseDatos.CrearParametro("@IdTipoAjuste", catalogoAjustes.EsActivo, ParameterDirection.Input);
+                }
+
+                catalogoAjustes.Descripcion = catalogoAjustes.Descripcion.TrimEnd().TrimStart();
+
+                if ( catalogoAjustes.Descripcion.Length > 0)
+                {
+                    IDataParameter parametroEsActivo = _baseDatos.CrearParametro("@IdTipoAjuste", catalogoAjustes.EsActivo, ParameterDirection.Input);
+                }
+
+                IDataReader lector = comando.ExecuteReader();
+
+                while (lector.Read())
+                {
+                    CatTipoAjustes catTipoAjustes = new CatTipoAjustes();
+
+                    catTipoAjustes.IdTipoAjuste = (int)lector["IdTipoAjuste"];
+                    catTipoAjustes.Descripcion = lector["Descripcion"].ToString();
+                    catTipoAjustes.IdUsuario = (int)lector["IdUsuario"];
+                    catTipoAjustes.EsActivo = (bool)lector["EsActivo"];
+
+                    ListaTipoAjustes.Add(catTipoAjustes);
+                }
+
+                lector.Close();
+
+                return null;
+            }
+            catch (Exception excepcionCapturada)
+            {
+                ExcepcionPersonalizada excepcion = new ExcepcionPersonalizada("No fue posible obtener la lista de los elementos del catálogo de tipos de ajustes.", excepcionCapturada);
+                return excepcion;
+
+            }
+            finally
+            {
+                if (conexion != null && conexion.State != ConnectionState.Closed)
+                    conexion.Close();
+                    conexion.Dispose();
+            }
+
         }
     }
 }
